f1614557af10aab999a58ebf2e79c9f1e44eb091
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 12:40:53 -0300

    Update AppMedia.java

--- AtividadesJava/Aula02/Exercício4/AppMedia.java
+++ AtividadesJava/Aula02/Exercício4/AppMedia.java
@@ -1,1 +1,10 @@
+package Aula02.Exercício4;
 
+public class AppMedia {
+    
+    public static void main(String[] args) {
+        
+        Media m1 = new Media();
+        m1.CalcularMedia();
+    }
+}
