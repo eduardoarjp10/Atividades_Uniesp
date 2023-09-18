2a80a97467508c1a0e6e055f7fa23ccf23449652
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 12:36:23 -0300

    Update appCalculadora.java

--- AtividadesJava/Aula02/Exercício2/appCalculadora.java
+++ AtividadesJava/Aula02/Exercício2/appCalculadora.java
@@ -1,1 +1,11 @@
+package Aula02.Exercício2;
 
+public class appCalculadora {
+    public static void main(String[] args) {
+
+        calculadora c1 = new calculadora();
+
+        c1.calc();
+        
+    }
+}
