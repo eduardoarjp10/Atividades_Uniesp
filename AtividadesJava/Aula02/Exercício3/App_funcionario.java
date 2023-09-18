8f085271bc10427dd6c9b1e80884c48c686699a4
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 12:39:18 -0300

    Update App_funcionario.java

--- AtividadesJava/Aula02/Exercício3/App_funcionario.java
+++ AtividadesJava/Aula02/Exercício3/App_funcionario.java
@@ -1,1 +1,11 @@
+package Aula02.Exercício3;
 
+public class App_funcionario {
+    public static void main(String[] args) {
+        
+        Funcionario f1 = new Funcionario(4567, "Eduardo Araujo", 1500);
+        f1.Status();
+        f1.Calcular_INSS();
+        f1.Status();
+    }
+}
