d131bfd238c0512abb82c6916713641a94b252c6
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 12:09:58 -0300

    Update Exercício2.java

--- AtividadesJava/Aula01/Exercício2.java
+++ AtividadesJava/Aula01/Exercício2.java
@@ -1,1 +1,31 @@
+package Aula01;
+import java.util.Scanner;
 
+public class Ex02 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.println("Digite sua nota: ");
+        double nota = sc.nextDouble();
+
+        
+        System.out.println("Digite sua nota: ");
+        double nota2 = sc.nextDouble();
+
+        
+        System.out.println("Digite sua nota: ");
+        double nota3 = sc.nextDouble();
+        
+
+        double Media = (nota + nota2 + nota3)/3;
+
+        System.out.println("A media do aluno foi de " + Media);
+
+
+        
+        sc.close();
+    }
+}
