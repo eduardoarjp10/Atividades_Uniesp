5c5005b4372a1be072c67fde7eb545101132b874
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 12:43:02 -0300

    Update Exercício1.java

--- AtividadesJava/Aula03/Exercício1.java
+++ AtividadesJava/Aula03/Exercício1.java
@@ -1,1 +1,12 @@
+package Aula03;
 
+
+public class Exercício1 {
+
+    public static void main(String[] args) {
+        
+        ex01Metodo e1 = new ex01Metodo(89, 1.81);
+        e1.IMC();
+
+    }
+}
