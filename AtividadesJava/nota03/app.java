a39ab4d6b9bc328c5fdbb823fc6db9d6d2310dbc
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 08:48:19 -0300

    Update app.java

--- AtividadesJava/nota03/app.java
+++ AtividadesJava/nota03/app.java
@@ -1,1 +1,11 @@
+package nota03;
 
+public class app {
+
+    public static void main(String[] args) {
+        
+        cadastro c1 = new cadastro(227, "caju", 5);
+        c1.Vender();
+    }
+
+}
