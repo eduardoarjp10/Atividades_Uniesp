90a569d3a3e2642e470f26cbf050dd90714bee37
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 12:10:37 -0300

    Update Exercício3.java

--- AtividadesJava/Aula01/Exercício3.java
+++ AtividadesJava/Aula01/Exercício3.java
@@ -1,1 +1,17 @@
+package Aula01;
 
+public class Ex03 {
+
+    public static void main(String[] args) {
+        
+        //for de maneira decresente 
+        for( int i= 5; i>0; i--){
+            System.out.println(i);
+        }
+
+        //FOR CRESCENTE
+        for( int j =0; j<=5; j++){
+            System.out.println(j);
+        }
+    }
+}
