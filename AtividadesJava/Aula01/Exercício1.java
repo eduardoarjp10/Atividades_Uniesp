a2c173863e716948775862f0475c3a0ac2a7d3c9
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 12:09:23 -0300

    Update Exercício1.java

--- AtividadesJava/Aula01/Exercício1.java
+++ AtividadesJava/Aula01/Exercício1.java
@@ -1,1 +1,32 @@
+package Aula01;
+import java.util.Scanner;
 
+public class Ex01 {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor do seu salario: ");
+        double Salario = sc.nextDouble();
+        if(Salario > 950 &&Salario <=1250 ){
+            Salario = Salario + (Salario *10)/100;
+            System.out.println("O salario do funcionario corresponde a " + Salario);
+        }else if(Salario >= 1251 &&Salario <= 1500){
+            Salario = Salario + (Salario * 15)/100;
+            System.out.println("O salario do funcionario corresponde a " + Salario);
+        }else if(Salario > 1500 &&Salario <=- 2100){
+            Salario = Salario + (Salario + 20)/100;
+            System.out.println("O salario do funcionario corresponde a " + Salario);
+        }else if( Salario > 2100){
+            Salario = Salario + (Salario + 25)/100;
+            System.out.println("O salario do funcionario corresponde a " + Salario);
+        }else{
+            System.out.println("O valor não foi encontrado no nosso banco de dados");
+        }
+        
+
+        sc.close();
+
+
+    }
+}
