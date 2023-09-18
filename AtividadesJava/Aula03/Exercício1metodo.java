b87494bddb85e9a14f800cebee793a9411b54c8a
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 12:43:54 -0300

    Update Exercício1metodo.java

--- AtividadesJava/Aula03/Exercício1metodo.java
+++ AtividadesJava/Aula03/Exercício1metodo.java
@@ -1,1 +1,38 @@
+package Aula03;
 
+public class Exercício1metodo {
+
+    private double peso;
+    private double altura;
+    
+    public ex01Metodo(double peso, double altura) {
+		this.peso = peso;
+		this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void IMC(){
+        double imc = peso/(altura*altura);
+        System.out.println("O imc do individuo é de: " + imc);    
+    }
+
+    
+    
+    
+    
+}
