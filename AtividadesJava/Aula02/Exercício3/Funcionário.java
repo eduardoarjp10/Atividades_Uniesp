a141b65459ed1ee22538f0f8f2012dbcf95b1916
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-18 12:40:11 -0300

    Update Funcionário.java

--- AtividadesJava/Aula02/Exercício3/Funcionário.java
+++ AtividadesJava/Aula02/Exercício3/Funcionário.java
@@ -1,1 +1,26 @@
+package Aula02.Exercício3;
 
+public class Funcionario {
+    int matricula;
+    String nome;
+    double salario_Bruto;
+    double NovoSalario;
+
+    public Funcionario(int matricula, String nome, double salario_Bruto) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.salario_Bruto = salario_Bruto;
+        this.NovoSalario = 0.0;
+    }
+
+    public void Calcular_INSS() {
+        double Df = salario_Bruto - (salario_Bruto * 30) / 100;
+        NovoSalario = salario_Bruto - Df; 
+    }
+
+    public void Status() {
+        System.out.println("Funcionario: " + nome);
+        System.out.println("Salario bruto: " + salario_Bruto);
+        System.out.println("Declaração do inss: " + NovoSalario);
+    }
+}
