995be2cbe7169b413295bf4bf836156e9c6db33b
Eduardo Araújo Pereira <144017720+eduardoarjp10@users.noreply.github.com>
2023-09-19 16:11:44 -0300

    Update NotaDeAulaApp.java

--- AtividadesJava/Nota05/NotaDeAulaApp.java
+++ AtividadesJava/Nota05/NotaDeAulaApp.java
@@ -1,1 +1,10 @@
+package Aula05;
 
+public class NotaDeAulaApp {
+    
+    public static void main(String[] args) {
+        
+        Nota_de_aula a1 = new NotaDeAula("Toyota", "Hillux");
+        a1.Status();
+    }
+}
